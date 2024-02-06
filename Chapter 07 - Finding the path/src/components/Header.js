56d2681bb40a1b7b27ef2d04d74674b1eab0ce55
YashRuikar <yashruikar04@gmail.com>
2024-02-06 22:28:41 +0530

    Chapter 07 - Finding the path

---
+++ Chapter 07 - Finding the path/src/components/Header.js
@@ -0,0 +1,49 @@
+import { LOGO_URL } from "../utils/constant";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  // State Variable
+  const [btnName, setBtnName] = useState("Login");
+  console.log("Header Render");
+
+  // if no dependency array => useEffect is called on every render
+  // if dependency array is empty = [] => useEffect is called on initial render(just once)
+  // if dependency array is [btnName] => called everytime btnName is updated
+
+  useEffect(() => {
+    console.log("useEffect called");
+  }, [btnName]);
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
