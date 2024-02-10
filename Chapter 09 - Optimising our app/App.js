d17422e9e6b5e3e5515ef1a6b9c9897d292e2b12
YashRuikar <yashruikar04@gmail.com>
2024-02-10 18:25:31 +0530

    Chapter 09 - Optimising our app

---
+++ Chapter 09 - Optimising our app/App.js
@@ -0,0 +1,73 @@
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./src/components/Header";
+import Body from "./src/components/Body";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+//import About from "./src/components/About";
+import Contact from "./src/components/Contact";
+import Error from "./src/components/Error";
+import RestaurantMenu from "./src/components/RestaurantMenu";
+import RestaurantMenu from "./src/components/RestaurantMenu";
+import Grocery from "./src/components/Grocery";
+
+// Chunking
+// Code Splitting
+// Dynamic Bundling
+// lazy loading
+// on demand loading
+// dynamic import
+
+const Grocery = lazy(() => import("./src/components/Grocery"));
+
+const About = lazy(() => import("./src/components/About"));
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/restaurant/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter} />);
